a8def202f9a0c44b5a13cfaf1b29256c3061fd82
Leoric01 <troglodit.nick@seznam.cz>
2024-10-14 17:22:03 +0200

    new complex eshop practice

---
+++ src/main/java/com/leoric/ecommerceshopbe/ECommerceShopBeApplication.java
@@ -0,0 +1,13 @@
+package com.leoric.ecommerceshopbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceShopBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceShopBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leoric/ecommerceshopbe/ECommerceShopBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leoric.ecommerceshopbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceShopBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
