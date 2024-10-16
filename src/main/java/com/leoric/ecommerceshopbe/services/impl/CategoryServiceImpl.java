f5df17c2bf54ee0d155c161401002e59f38fe54b
Leoric01 <troglodit.nick@seznam.cz>
2024-10-16 22:21:21 +0200

    started seller

--- src/main/java/com/leoric/ecommerceshopbe/services/impl/CategoryServiceImpl.java
+++ src/main/java/com/leoric/ecommerceshopbe/services/impl/CategoryServiceImpl.java
@@ -15,6 +15,11 @@ public class CategoryServiceImpl implements CategoryService {
     private final CategoryRepository categoryRepository;
 
     @Override
+    public Category createCategory(Category category) {
+        return null;
+    }
+
+    @Override
     public List<Category> findAll() {
         return categoryRepository.findAll();
     }
