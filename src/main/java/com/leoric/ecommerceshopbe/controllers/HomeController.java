148d24317fdf2b19702aa062f5a9991afb96befc
Leoric01 <troglodit.nick@seznam.cz>
2024-10-16 12:20:54 +0200

    implemented consistent way of output and handling errors

--- src/main/java/com/leoric/ecommerceshopbe/controllers/HomeController.java
+++ src/main/java/com/leoric/ecommerceshopbe/controllers/HomeController.java
@@ -1,12 +1,13 @@
 package com.leoric.ecommerceshopbe.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
-//    @GetMapping("/")
-//    public String home() {
-//        return "Hello World";
-//    }
+    @GetMapping("/home/index")
+    public String home() {
+        return "Hello World";
+    }
 }
