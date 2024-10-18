2bf6da1748d484e66ba7fc4edf4defb16833d82e
Leoric01 <troglodit.nick@seznam.cz>
2024-10-18 16:34:06 +0200

    started with order

--- src/main/java/com/leoric/ecommerceshopbe/services/impl/OrderServiceImpl.java
+++ src/main/java/com/leoric/ecommerceshopbe/services/impl/OrderServiceImpl.java
@@ -1,12 +1,17 @@
 package com.leoric.ecommerceshopbe.services.impl;
 
+import com.leoric.ecommerceshopbe.models.Address;
+import com.leoric.ecommerceshopbe.models.Cart;
 import com.leoric.ecommerceshopbe.models.Order;
+import com.leoric.ecommerceshopbe.models.constants.OrderStatus;
 import com.leoric.ecommerceshopbe.repositories.OrderRepository;
 import com.leoric.ecommerceshopbe.services.interfaces.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 @RequiredArgsConstructor
@@ -14,15 +19,35 @@ public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepository;
 
+
     @Override
-    public List<Order> findAll() {
-        return orderRepository.findAll();
+    public Set<Order> createOrder(Authentication connectedUser, Address address, Cart cart) {
+        return Set.of();
     }
 
     @Override
-    public Order findById(Long id) {
-        return orderRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Order not found"));
+    public Order findOrderById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Order> usersOrderHistory(Authentication connectedUser) {
+        return List.of();
+    }
+
+    @Override
+    public List<Order> sellersOrder(Long sellerId) {
+        return List.of();
+    }
+
+    @Override
+    public Order updateOrderStatus(Long orderId, OrderStatus orderStatus) {
+        return null;
+    }
+
+    @Override
+    public Order cancelOrder(Long orderId, Authentication connectedUser) {
+        return null;
     }
 
     @Override
--- src/main/java/com/leoric/ecommerceshopbe/services/interfaces/OrderService.java
+++ src/main/java/com/leoric/ecommerceshopbe/services/interfaces/OrderService.java
@@ -1,16 +1,28 @@
 package com.leoric.ecommerceshopbe.services.interfaces;
 
+import com.leoric.ecommerceshopbe.models.Address;
+import com.leoric.ecommerceshopbe.models.Cart;
 import com.leoric.ecommerceshopbe.models.Order;
+import com.leoric.ecommerceshopbe.models.constants.OrderStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 public interface OrderService {
+    Set<Order> createOrder(Authentication connectedUser, Address address, Cart cart);
 
-    List<Order> findAll();
+    Order findOrderById(Long id);
 
-    Order findById(Long id);
+    List<Order> usersOrderHistory(Authentication connectedUser);
+
+    List<Order> sellersOrder(Long sellerId);
+
+    Order updateOrderStatus(Long orderId, OrderStatus orderStatus);
+
+    Order cancelOrder(Long orderId, Authentication connectedUser);
 
     Order save(Order entity);
 
