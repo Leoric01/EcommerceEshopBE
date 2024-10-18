7a1826cac4b4148b9a651f8f3d4dc139d1091f7e
Leoric01 <troglodit.nick@seznam.cz>
2024-10-18 08:42:53 +0200

    need anotha interface for pageable

--- src/main/java/com/leoric/ecommerceshopbe/repositories/ProductRepository.java
+++ src/main/java/com/leoric/ecommerceshopbe/repositories/ProductRepository.java
@@ -2,8 +2,9 @@ package com.leoric.ecommerceshopbe.repositories;
 
 import com.leoric.ecommerceshopbe.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, JpaSpecificationExecutor<Product> {
 }
