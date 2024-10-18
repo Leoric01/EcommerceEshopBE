87779ec3c8674d99579c687a33230700ab7e5086
Leoric01 <troglodit.nick@seznam.cz>
2024-10-18 09:56:33 +0200

    implemented plenty endpoints related to seller and product

--- src/main/java/com/leoric/ecommerceshopbe/controllers/ProductController.java
+++ src/main/java/com/leoric/ecommerceshopbe/controllers/ProductController.java
@@ -5,11 +5,11 @@ import com.leoric.ecommerceshopbe.response.common.Result;
 import com.leoric.ecommerceshopbe.services.interfaces.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 import static org.springframework.http.HttpStatus.OK;
 
@@ -25,6 +25,31 @@ public class ProductController {
     public ResponseEntity<Result<Product>> getProduct(@PathVariable Long productId) {
         Product product = productService.findProductById(productId);
         Result<Product> response = Result.success(product, "Product's details fetched succesfully", OK.value());
+        return ResponseEntity.status(OK).body(response);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<Result<List<Product>>> searchProduct(@RequestParam(required = false) String query) {
+        List<Product> product = productService.searchProducts(query);
+        Result<List<Product>> response = Result.success(product, "Product's searched and fetched succesfully", OK.value());
+        return ResponseEntity.status(OK).body(response);
+
+    }
+
+    @GetMapping("")
+    public ResponseEntity<Result<Page<Product>>> getAllProducts(
+            @RequestParam(required = false) String category,
+            @RequestParam(required = false) String brand,
+            @RequestParam(required = false) String colors,
+            @RequestParam(required = false) String sizes,
+            @RequestParam(required = false) Integer minPrice,
+            @RequestParam(required = false) Integer maxPrice,
+            @RequestParam(required = false) Integer minDiscount,
+            @RequestParam(required = false) String sort,
+            @RequestParam(required = false) String stock,
+            @RequestParam(defaultValue = "0") Integer pageNumber) {
+        Page<Product> products = productService.getAllProducts(category, brand, colors, sizes, minPrice, maxPrice, minDiscount, sort, stock, pageNumber);
+        Result<Page<Product>> response = Result.success(products, "Products fetched succesfully", OK.value());
         return ResponseEntity.status(OK).body(response);
     }
 }
---
+++ src/main/java/com/leoric/ecommerceshopbe/controllers/SellerProductController.java
@@ -0,0 +1,60 @@
+package com.leoric.ecommerceshopbe.controllers;
+
+import com.leoric.ecommerceshopbe.models.Product;
+import com.leoric.ecommerceshopbe.models.Seller;
+import com.leoric.ecommerceshopbe.requests.CreateProductReq;
+import com.leoric.ecommerceshopbe.response.common.Result;
+import com.leoric.ecommerceshopbe.services.interfaces.ProductService;
+import com.leoric.ecommerceshopbe.services.interfaces.SellerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/sellers/products")
+public class SellerProductController {
+    private final ProductService productService;
+    private final SellerService sellerService;
+
+    @GetMapping("")
+    public ResponseEntity<Result<List<Product>>> getProductsBySellerId(Authentication connectedUser) {
+        Seller seller = (Seller) connectedUser.getPrincipal();
+        List<Product> product = productService.getProductBySellerId(seller.getId());
+        Result<List<Product>> response = Result.success(product, "Product's searched by connected users id and fetched succesfully", OK.value());
+        return ResponseEntity.status(OK).body(response);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Result<Product>> createProduct(Authentication connectedUser,
+                                                         @RequestBody CreateProductReq request) {
+        Seller seller = (Seller) connectedUser.getPrincipal();
+        Product product = productService.createProduct(request, seller);
+        Result<Product> response = Result.success(product, "Product created succesfully", CREATED.value());
+        return ResponseEntity.status(CREATED).body(response);
+    }
+
+    @DeleteMapping("/{productId}")
+    public ResponseEntity<Result<Void>> deleteProduct(@PathVariable Long productId) {
+        productService.deleteById(productId);
+        Result<Void> response = Result.success("Product deleted succesfully", CREATED.value());
+        return ResponseEntity.status(CREATED).body(response);
+    }
+
+    @PutMapping("/{productId}")
+    public ResponseEntity<Result<Product>> updateProduct(@PathVariable Long productId,
+                                                         @RequestBody Product product) {
+        Product updatedProduct = productService.updateProduct(productId, product);
+        Result<Product> response = Result.success(updatedProduct, "Product updated succesfully", OK.value());
+        return ResponseEntity.status(CREATED).body(response);
+    }
+
+}
