1840660ff7e2551b45d1d3c8e44eb11665468b3b
Leoric01 <troglodit.nick@seznam.cz>
2024-11-03 15:05:22 +0100

    working that hibernate behavior - product has LocalDateTime issue

--- src/main/java/com/leoric/ecommerceshopbe/models/Product.java
+++ src/main/java/com/leoric/ecommerceshopbe/models/Product.java
@@ -1,5 +1,6 @@
 package com.leoric.ecommerceshopbe.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
@@ -46,6 +47,7 @@ public class Product {
 
     @CreationTimestamp
     @Column(nullable = false, updatable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime createdAt = LocalDateTime.now();
     //    @UpdateTimestamp
 //    private LocalDateTime updatedAt;
